8970da8fd39a25f6f00e2dbabed108d6d4002082
Reilly Hartigan <reilly.hartigan@yale.edu>
2019-04-03 00:07:29 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,5 @@
-numbers = [1,2,3]
-
 def square_array(array)
-  arr = []
-  array.each_char { |i| arr << i ** 2 }
-  arr
+  squared = []
+    array.each { |element| squared << element ** 2 }
+    squared
 end
